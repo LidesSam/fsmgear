618af274723e561f399671e2e53ce727bc85dd53
Santaigo A. Arrieta <lides.sam@gmail.com>
2026-09-23 02:14:13 -0300

    feat: basic editor that allow to visualize and add the states

--- addons/fsmgear/fsm_editor.gd
+++ addons/fsmgear/fsm_editor.gd
@@ -2,25 +2,76 @@
 extends Control
 
 var editor_interface
-var statesNodes=[]	
+var statesNodes: Array[GraphNode] = []
 @onready var statesDisplay:GraphEdit=$GraphEdit
+var currentTarget=null
+@onready var targetNameLbl:Label=$sidebar/TargetLbl
+@onready var fsmStatesList:Control= $sidebar/fsmStates
+
+func _ready() -> void:
+	targetNameLbl=$sidebar/TargetLbl
+	
 func show_fsm(node):
-	$TargetLbl.text = node.name
+	currentTarget=null
+	currentTarget=node
+	targetNameLbl.text = node.name
+	current_target_data()
+	
+func current_target_data():
+	if !currentTarget:
+		return
 	var i = 0
-
-	for state in node.get_children():
+	for state in currentTarget.get_children():
 		i += 1
-
 		var stateDispNode: GraphNode = GraphNode.new()
 		stateDispNode.title = state.name
-		stateDispNode.position = Vector2(16, 16 + 128 * i)
-		stateDispNode.position_offset = Vector2(16, 16 + 128 * i)
+		#stateDispNode.position = Vector2(16, 16 + 48* i)
+		stateDispNode.position_offset = Vector2(16, 16 + 48 * i)
 
 		statesDisplay.add_child(stateDispNode)
+		statesNodes.push_back(stateDispNode)
+		var statelistlbl = Label.new()
+		statelistlbl.text= state.name
+		fsmStatesList.add_child(statelistlbl)
 	
 
-func clear_graph():
-	$TargetLbl.text = "No FSM"
+func clear_graph(clearTarget=true):
+	if clearTarget:
+		currentTarget=null
+		targetNameLbl.text = "No FSM"
 	for child in statesDisplay.get_children():
 		if child is GraphNode:#to prevent missin conection_layer
 			child.queue_free()
+	for child in fsmStatesList.get_children():
+		child.queue_free()
+
+
+func get_unique_state_name(stateName: String) -> String:
+	if not currentTarget:
+		return stateName
+	var newName= stateName
+	var count= 1
+
+	while currentTarget.has_node(newName):
+		newName = stateName + str(count)
+		count += 1
+
+	return newName
+
+func _on_add_pressed() -> void:
+	if currentTarget:
+		var stateName = $sidebar/newStateNameEditLine.text.strip_edges()
+		print(stateName)
+		if stateName.is_empty() :
+			stateName = "STATE%d" % (currentTarget.get_child_count() + 1)
+		else:
+			#duplicate state add number at the end
+			stateName= get_unique_state_name(stateName)
+		var fsmstate = preload("res://addons/fsmgear/source/FsmState.gd").new()
+		fsmstate.name = stateName
+		currentTarget.add_child(fsmstate)
+		fsmstate.owner = currentTarget.owner
+		clear_graph(false)
+		current_target_data()
+			
+		
--- addons/fsmgear/source/FsmTransition.gd
+++ addons/fsmgear/source/FsmTransition.gd
@@ -1,8 +1,9 @@
 @tool
 extends Node
 
-var condition = null
-var nexState=null
+@export var nexState: Node=null
+@export var condition: Callable 
+
 
 #used in 
 var canToSelf=false
@@ -17,7 +18,7 @@ func define(nstate,cond:Callable):
 	triggerName=cond.get_method()
 	pass
 
-func checkCondition(fsm):
+func check_condition(fsm):
 	if(condition.call()):
 		print("aowner:",fsm.actowner.name,"  triger:",triggerName," nstate:",nexState)
 		fsm.change_to_state(nexState)
